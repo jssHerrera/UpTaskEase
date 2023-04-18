efddf7c997280ab1b218744e24adca40dada1f74
Denis Herrera Espiritu <jsherr.6@gmail.com>
2023-04-17 21:34:40 -0500

    Modificar nombres de variables en petición PUT para editar tarea

--- frontEnd/src/components/TarjetaTarea/ContenTarea.jsx
+++ frontEnd/src/components/TarjetaTarea/ContenTarea.jsx
@@ -1,62 +1,56 @@
 import { LogosFigma } from '../../container/Icon'
-import { hashCode, intToRGB } from '../../helpers/coloresDinamicos'
+import { coloresDinamicos } from '../../helpers/coloresDinamicos'
 import { formateFecha2 } from '../../helpers/formateFecha'
+import { upperCaseFirs } from '../../helpers/UpperCase'
+import { DropTareas } from '../../container/DropTareas'
 import { useTarea } from '../../hooks/useTarea'
 
-export const ContenTarea = ({ tareaID, nombre, descripcion, prioridad, fechaEntrega }) => {
-  const { setModalOpcionTarea, setPoints, setTareaID } = useTarea()
+export const ContenTarea = ({ elem }) => {
+  const { tareaID, nombre, descripcion, prioridad, fechaEntrega } = elem
+  const { handleCloseDrop } = useTarea()
 
-  const fecha = formateFecha2(fechaEntrega)
-
-  const coloresPrioridad = {
-    Alta: 'hsla(3, 85%, 55%, 1)',
-    Media: 'hsla(162, 65%, 47%, 0.76)',
-    Baja: 'hsla(180, 1%, 30%, 1)'
+  const closeDrop = () => {
+    handleCloseDrop()
   }
-  const colorPrioridad = coloresPrioridad[prioridad]
+
+  const titulo = upperCaseFirs(nombre)
+  const contenido = upperCaseFirs(descripcion)
+  const fecha = formateFecha2(fechaEntrega)
+  const colors = coloresDinamicos(tareaID)
 
   return (
-
     <div
       className='input-btn input-form input-dark p-2 flex flex-col'
-      onContextMenu={(e) => {
-        e.preventDefault()
-        setModalOpcionTarea(true)
-        setTareaID({
-          tareaID,
-          nombre,
-          descripcion,
-          prioridad,
-          fechaEntrega
-        })
-        setPoints({ x: e.pageX, y: e.pageY })
-      }}
+      onClick={closeDrop}
     >
-      <div className='flex gap-2 items-center justify-between'>
-        <div className='flex gap-2 items-center '>
+      <div className='flex gap-2 '>
+        <div className='flex gap-2'>
           <span
             className='w-5 border border-gray-300 dark:border-gray-600 h-5 flex items-center justify-center rounded-md'
-            style={{ backgroundColor: intToRGB(hashCode(tareaID)) }}
+            style={{ backgroundColor: colors }}
           >
             <LogosFigma />
           </span>
-          <p className='font-semibold'>{nombre}</p>
         </div>
-        <span
-          className='min-w-[50px] min-h-[25px] h-full flex items-center justify-center text-white bg-red-500/70 px-1 border border-gray-500 rounded-md'
-          style={{ backgroundColor: colorPrioridad }}
-        >
-          {prioridad}
-        </span>
+        <div className='w-full'>
+          <div className='flex justify-between '>
+            <p className='font-semibold'>{titulo}</p>
+            <DropTareas elem={elem} />
+          </div>
+          <div className='pt-2'>
+            <p>
+              {contenido}
+            </p>
+            <p className='text-gray-500'>
+              Fecha de entrega: <span className='text-gray-400'>{fecha}</span>
+            </p>
+            <p className='text-gray-500'>
+              Prioridad: <span className='text-gray-400'>{prioridad}</span>
+            </p>
+          </div>
+        </div>
       </div>
-      <span>
-        {descripcion}
-      </span>
-      <div className='dark:text-gray-500 text-gray-400'>
-        <p>
-          fecha de entrega: <span className='text-gray-300'>{fecha}</span>
-        </p>
-      </div>
+
     </div>
   )
 }
