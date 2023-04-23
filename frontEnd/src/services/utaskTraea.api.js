5ce5834b011427407698adf9d9e33d7183f8f68f
Denis Herrera Espiritu <jsherr.6@gmail.com>
2023-04-22 21:48:51 -0500

    Agregar función para eliminar tarea usando clienteAxios y parámetro tareaId

--- frontEnd/src/services/utaskTraea.api.js
+++ frontEnd/src/services/utaskTraea.api.js
@@ -7,6 +7,7 @@ export const postNuevaTarea = async (datos = {}, config) => {
   const { data } = await clienteAxios.post('/tareas', datos, config)
   return data
 }
+
 // ==============================
 // editar Tarea
 // ==============================
@@ -14,5 +15,13 @@ export const putEditarTarea = async (tareaId = {}, config) => {
   const { nombre, descripcion, prioridad, fechaEntrega, tareaID } = tareaId
 
   const { data } = await clienteAxios.put(`/tareas/${tareaID}`, { nombre, descripcion, prioridad, fechaEntrega, tareaID }, config)
+  return data
+}
+
+// ==============================
+// editar Tarea
+// ==============================
+export const deleteTarea = async (tareaId = {}, config) => {
+  const { data } = await clienteAxios.delete(`/tareas/${tareaId}`, config)
   return data
 }
